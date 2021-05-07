78459b3d8b46b4245def9c6802e68b61d5a9b4af
钟小平 <zhongxiaoping@zhongxiaopingdeMacBook-Pro.local>
2021-05-07 22:04:44 +0800

    :fire:

---
+++ go.mod
@@ -0,0 +1,22 @@
+module web-app
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.38.35 // indirect
+	github.com/go-playground/validator/v10 v10.6.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
+	github.com/klauspost/compress v1.12.2 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/onsi/ginkgo v1.16.1 // indirect
+	github.com/onsi/gomega v1.11.0 // indirect
+	github.com/shenghui0779/yiigo v1.2.5 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mongodb.org/mongo-driver v1.5.2 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210507014357-30e306a8bba5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/shenghui0779/yiigo"
+)
+
+var sessionStore = sessions.NewCookieStore([]byte("t09-secret"))
+var client *redis.Client
+var templates *template.Template
+
+func main() {
+	r := mux.NewRouter()
+	templates = template.Must(template.ParseGlob("./templates/*.html"))
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	r.HandleFunc("/login", loginGetHandler).Methods("GET")
+	r.HandleFunc("/login", loginPostHandler).Methods("POST")
+	r.HandleFunc("/register", registerGetHandler).Methods("GET")
+	r.HandleFunc("/register", registerPostHandler).Methods("POST")
+	http.Handle("/", r)
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+	env := yiigo.Env("app.env").String()
+	fmt.Println(env)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type Todo struct {
+	Title string
+	Items []string
+}
+
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("hhhh")
+
+	values, _ := client.LRange("todos", 0, 10).Result()
+	templates.ExecuteTemplate(w, "index.html", Todo{
+		Title: "首页",
+		Items: values,
+	})
+
+}
+func loginGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login.html", "欢迎来到登陆页")
+}
+func registerGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "register.html", "欢迎来到注册页")
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	title := r.FormValue("title")
+
+	if len(title) == 0 {
+		log.Println(title)
+		return
+	}
+	client.LPush("todos", title)
+	http.Redirect(w, r, "/", http.StatusFound)
+
+}
+func loginPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.FormValue("username")
+	pwd := r.FormValue("pwd")
+	log.Println(username, pwd)
+	store := sessions.NewSession(sessionStore, username)
+	sessionStore.Save(r, w, store)
+
+}
+func registerPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.FormValue("username")
+	pwd := r.FormValue("pwd")
+	client.HSet("user", "username", username)
+	client.HSet("user", "pwd", pwd)
+	http.Redirect(w, r, "/login", http.StatusFound)
+
+}
